eadf849d195a48de52bd65528b22174a64571252
Leandro Gonçalves <lleandrogon2004@gmail.com>
2026-07-09 22:09:46 -0300

    fix: Ajuste do código para uma Azure Function

--- src/silver.py
+++ src/silver.py
@@ -1,48 +1,54 @@
 import pandas as pd
-import duckdb
-import re
 import os
+import azure.functions as func
 from dotenv import load_dotenv
 
-load_dotenv()
+app = func.FunctionApp()
 
-account_name = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
-account_key = os.getenv("AZURE_STORAGE_ACCOUNT_KEY")
+@app.route(route = "etl")
+def etl(req: func.HttpRequest):
+    load_dotenv()
 
-container_bronze = "bronze"
-file_bronze = "inscricoes.csv"
-storage_options = {'account_name': account_name, 'account_key': account_key}
+    account_name = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
+    account_key = os.getenv("AZURE_STORAGE_ACCOUNT_KEY")
 
-azure_path = f"az://{container_bronze}/{file_bronze}"
+    container_bronze = "bronze"
+    file_bronze = "inscricoes.csv"
+    storage_options = {'account_name': account_name, 'account_key': account_key}
 
-df = pd.read_csv(
-    azure_path,
-    storage_options = storage_options
-)
+    azure_path = f"az://{container_bronze}/{file_bronze}"
 
-df["email"] = df["email"].str.strip().str.lower()
-
-df["cidade"] = df["cidade"].str.strip().str.title()
-
-df["data_cadastro"] = pd.to_datetime(
-    df["data_cadastro"]
-).dt.date
-
-container_silver = "silver"
-file_silver = "inscricoes_limpas.parquet"
-path_silver = f"az://{container_silver}/{file_silver}"
-
-try:
-    print("Iniciando a gravação dos dados na Silver")
-
-    df.to_parquet(
-        path_silver,
-        storage_options = storage_options,
-        index = False
+    df = pd.read_csv(
+        azure_path,
+        storage_options = storage_options
     )
 
-    print("✅ Dados gravados com sucesso na camada Silver!")
-except:
-    print("❌ Gravação de dados na Silver falhou!")
+    df["email"] = df["email"].str.strip().str.lower()
 
+    df["cidade"] = df["cidade"].str.strip().str.title()
 
+    df["data_cadastro"] = pd.to_datetime(
+        df["data_cadastro"]
+    ).dt.date
+
+    container_silver = "silver"
+    file_silver = "inscricoes_limpas.parquet"
+    path_silver = f"az://{container_silver}/{file_silver}"
+
+    try:
+        print("Iniciando a gravação dos dados na Silver")
+
+        df.to_parquet(
+            path_silver,
+            storage_options = storage_options,
+            index = False
+        )
+
+        print("✅ Dados gravados com sucesso na camada Silver!")
+    except:
+        print("❌ Gravação de dados na Silver falhou!")
+
+    return func.HttpResponse(
+        "ETL executado com sucesso!",
+        status_code = 200
+    )
